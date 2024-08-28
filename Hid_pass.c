b1961257e001c54d75fbf2e8d82ea86c5eb2fb44
Saurabh Jadhav <108616952+Jadhavsau777@users.noreply.github.com>
2024-08-28 16:44:24 +0530

    Update Hid_pass.c

--- Hid_pass.c
+++ Hid_pass.c
@@ -1,1 +1,36 @@
 
+
+  
+ 
+    #include<stdio.h>
+    #include<conio.h>
+    
+    int main()
+    {
+     int ind=0;
+     char pass[20],ch;
+     
+     printf("Enter Password: ");
+      
+     //Initiate While Loop To Start Filling Our String
+     // To avoid that user has pressed Enter Key make ch!=10(ASCII value of ENTER)
+      
+     while(ch!=10)
+     {
+       ch = getch();
+       pass[ind] = ch;
+       ind++;
+       //Print * Every Time You Type A Letter
+       printf("*");
+     }
+      
+     pass[ind] ='\0';
+      
+     //Print The String (pass)
+     printf("\nPassword Is: %s",pass);
+     
+     getch();
+     
+     
+    }     
+     
